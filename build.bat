cc9228aaa53df91acbb7cfc6c969c6165dbc9d31
Movie Vertigo <movievertigo@gmail.com>
2024-02-02 21:06:20 +0000

    Tidied up linker list

--- build.bat
+++ build.bat
@@ -7,5 +7,5 @@ cd obj
 ..\buildtools\ez80asm -cpu:ez80f92 ..\%name%.asm
 ..\buildtools\ez80cc -cpu:ez80f92 -optspeed ..\main.c
 ..\buildtools\ez80asm -cpu:ez80f92 main.src
-..\buildtools\ez80link CHANGE STRSECT is CODE CHANGE DATA is CODE CHANGE BSS is CODE CHANGE TEXT is CODE LOCATE CODE AT 262144 -format=srec %name%=%name%.obj,main.obj,agonmos.obj,agonlib.obj,..\components\frameset.obj,..\components\frameset0.obj,..\components\lneg.obj,..\components\ldivu.obj,..\components\ldivs.obj,..\components\ladd.obj,..\components\lsub.obj,..\components\lcmpu.obj,..\components\lmulu.obj,..\components\itol.obj,..\components\stoi.obj
+..\buildtools\ez80link CHANGE STRSECT is CODE CHANGE DATA is CODE CHANGE BSS is CODE CHANGE TEXT is CODE LOCATE CODE AT 262144 -format=srec %name%=%name%.obj,main.obj,agonmos.obj,agonlib.obj,..\components\frameset.obj,..\components\frameset0.obj,..\components\lneg.obj,..\components\ldivu.obj,..\components\ldivs.obj,..\components\ladd.obj,..\components\lsub.obj,..\components\lcmpu.obj,..\components\lmulu.obj,..\components\itol.obj,..\components\imulu.obj,..\components\idivs.obj,..\components\idivu.obj,..\components\irems.obj,..\components\iremu.obj,..\components\ineg.obj,..\components\stoi.obj,..\components\strlen.obj
 ..\buildtools\srec_cat -Disable_Sequence_Warnings %name%.srec -offset -0x40000 -o ..\bin\%name%.bin -Binary
